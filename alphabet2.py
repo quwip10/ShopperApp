665ff448ba6c87dfc6c5b61dc39d34745351d003
Atlas <26572979+quwip10@users.noreply.github.com>
2019-09-24 15:00:19 -0400

    Delete alphabet2.py

--- alphabet2.py
+++
@@ -1,6 +0,0 @@
-# Test alphabet
-
-def alphabet(stringy):
-    return ' '.join([str((ord(letter.upper()) - 64)) for letter in stringy if ord(letter.upper()) > 64 and ord(letter.upper()) <= 90])
-
-print(alphabet(input("enter string")))

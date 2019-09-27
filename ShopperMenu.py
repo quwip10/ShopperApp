0b05b77521fb6b44cb9ef45768fa0cf54db9029a
Alex Heidenreich <atlasalex@Alexs-MacBook-Air.local>
2019-09-26 21:44:57 -0400

    Created ShopperMenu class file

---
+++ ShopperMenu.py
@@ -0,0 +1,54 @@
+class ShopperMenu():
+    '''Displays and validates menu input'''
+
+    def __init__(self):
+        pass
+
+
+    def print_menu(cart):
+        '''prints a menu and edits shopping cart item'''
+
+        user_input = None
+        valid_choices = ('a', 'r', 'c', 'i', 'o', 'q')
+
+        print('''
+    MENU
+    a - Add item to cart
+    r - Remove item from cart
+    c - Change item quantity
+    i - Output items' descriptions
+    o - Output shopping cart
+    q - Quit
+    ''')
+
+        while user_input not in valid_choices:
+            user_input = input("Choose an option:\n")
+
+            if user_input == 'a':
+                print("ADD ITEM TO CART")
+                cart.add_item(get_item_object())
+
+            elif user_input == 'r':
+                print("REMOVE ITEM FROM CART")
+                cart.remove_item(input("Enter name of item to remove:\n"))
+
+            elif user_input == 'c':
+                print("CHANGE ITEM QUANTITY")
+                temp_item = ItemToPurchase(
+                        item_name=input("Enter the item name:\n"),
+                        item_quantity=int(input("Enter the new quantity:\n")))
+                cart.modify_item(temp_item)
+
+            elif user_input == 'i':
+                print("OUTPUT ITEMS' DESCRIPTIONS")
+                print("{}'s Shopping Cart - {}".format(
+                        cart.customer_name, cart.current_date))
+                print()
+                print("Item Descriptions")
+                cart.print_descriptions()
+
+            elif user_input == 'o':
+                print("OUTPUT SHOPPING CART")
+                cart.print_total()
+
+        return user_input
